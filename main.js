fda262b66c7902716e5df6499fadb093720eda8e
Precious Ogoru <40835806+MrOgoru@users.noreply.github.com>
2020-04-13 11:05:32 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector('.down').addEventListener('click', () => document.querySelector('.shown')
+    .classList.toggle('open'));
+
+document.querySelector('.main-btn').addEventListener('click', () => document.querySelector(
+        '.mobile-nav')
+    .classList.toggle('show'));
